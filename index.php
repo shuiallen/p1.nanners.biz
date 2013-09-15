d89658035baa28843af6464faa67d677f958da80
Shirley Hui <shuiallen@gmail.com>
2013-09-15 14:23:00 -0400

    Added external css page and page content

--- index.php
+++ index.php
@@ -1,6 +1,47 @@
-<b>Project 1 for CSCI-E15</b>
+<!DOCTYPE html>
+<html>
 
+<head>
+<!--Use UTF-8 for character encoding-->
+<meta charset="UTF-8">
+<!--CSS in an external file-->
+<link rel="stylesheet" type="text/css" href="p1.css">
+<!--A nice title for the HTML document-->
+<title>Project 1 for CSCI-E15</title>
+</head>
+
+<body>
+<h1><b>Project 1 for CSCI-E15</b></h1>
+<h2>Introduction</h2>
 <p>My name is Shirley Hui</p>
+<h2>Bio</h2>
 
-<p>I am using Emacs, although I have also installed Sublime Text to play around with it.</p>
+<p>
+I am a Senior Software Engineer, Technical Project Lead and Project Manager.
+
+I have worked on applications (Rational ClearCase/ClearQuest and Jazz Platform products - Requirements Management, Product Management and Quality Management) and compilers (COBOL, Ada, FORTRAN - both front-end and vector and parallel optimizers). 
+
+I've played a role in every aspect of the software development lifecycle including planning and requirements gathering, development, testing, documentation, training, support, pre-Sales and post-Sales activities.
+</p>
+<p> 
+Despite all that experience, I've never built a website from scratch.
+
+</p>
+<p>  I know HTML and I'm learning the intricacies of CSS and what's different in HTML5
+</p>
+<p>
+I'm married, with 3 sons (16, 20, 24), originally from New York City, now from Natick.
+</p>
+<p>
+My personal passions are my family, cooking (from scratch and fresh ingredients, as much as possible), gardening and knitting.
+
+</p>
+
+<h2>Editor</h2>
+<p>I am using Emacs, although I plan to try Sublime Text.</p>
+</body>
+
+</html>
+
+
 

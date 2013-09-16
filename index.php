f463bba01e4752a2546f00bfe8d8b2db2732c14d
Shirley Hui <shuiallen@gmail.com>
2013-09-16 14:18:11 -0400

    Add changes to the page

--- index.php
+++ index.php
@@ -12,28 +12,26 @@
 
 <body>
 <h1><b>Project 1 for CSCI-E15</b></h1>
-<h2>Introduction</h2>
-<p>My name is Shirley Hui</p>
+<h2>Name</h2>
+<p>Shirley Hui</p>
 <h2>Bio</h2>
 
 <p>
 I am a Senior Software Engineer, Technical Project Lead and Project Manager.
 
-I have worked on applications (Rational ClearCase/ClearQuest and Jazz Platform products - Requirements Management, Product Management and Quality Management) and compilers (COBOL, Ada, FORTRAN - both front-end and vector and parallel optimizers). 
-
-I've played a role in every aspect of the software development lifecycle including planning and requirements gathering, development, testing, documentation, training, support, pre-Sales and post-Sales activities.
+I have worked on applications (Rational ClearCase/ClearQuest and Jazz Platform products - Requirements Management, Product Management and Quality Management) and language compilers (COBOL, Ada, FORTRAN, both front-end and vector and parallel optimizers). I've played a role in almost every aspect of the software development lifecycle.
 </p>
-<p> 
-Despite all that experience, I've never built a website from scratch.
 
-</p>
-<p>  I know HTML and I'm learning the intricacies of CSS and what's different in HTML5
+<p>I know a bunch about databases and web applications - though mostly server side, some dabbling in Dojo/Ajax, Javascript. I know basic HTML (mostly know where to look up what I don't know) and I'm learning the intricacies of CSS and what's different in HTML5.  I've never built a real website from scratch.
 </p>
 <p>
 I'm married, with 3 sons (16, 20, 24), originally from New York City, now from Natick.
 </p>
 <p>
-My personal passions are my family, cooking (from scratch and fresh ingredients, as much as possible), gardening and knitting.
+My personal passions are my family, cooking (from scratch and fresh ingredients, as much as possible), gardening and knitting. Here are a couple of highlights from this spring's garden:
+</p><p>
+<img src="pinkpeony.jpg" alt="Pink Peony" width="304" height="228">
+<img src="saponaria.jpg" alt="Saponaria" width="304" height="228">
 
 </p>
 
